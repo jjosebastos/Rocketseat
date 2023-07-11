9ecd5c2be682601bcced1c24666170d8f0dd7f5d
José Bastos <josebezerrabastos@gmail.com>
2023-07-10 22:54:47 -0300

    Manipulação de strings e numbers. Manipulação de arrays e métodos.

---
+++ Discover/11 - JavaScript/manipulando dados/script.js
@@ -0,0 +1,7 @@
+/*
+    Prototype
+
+    * prototype-based language: todo e qualquer valor (string, number, boolean) já tem uma prototipagem prévia onde é possível acessar t
+    * prototype chain
+    * __proto__
+ */
